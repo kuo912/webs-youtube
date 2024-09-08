60a9437ca7a431e810499728b13ef8c03537fd40
kuo912 <rkskek7938@naver.com>
2024-09-08 20:08:12 +0900

    페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ import Main from '../components/section/Main'
 
 const Home = () => {
   return (
-    <div>
-      <Main>
+    <Main 
+      title = "웹스토리보이 유튜브"
+      description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
         Home
-      </Main>
-    </div>
+    </Main>
   )
 }
 
