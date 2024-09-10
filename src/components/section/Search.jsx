2732d321a56bd4bd7a197be3406165d5adc316ef
kuo912 <rkskek7938@naver.com>
2024-09-10 18:04:38 +0900

    추천영상 작업

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -4,17 +4,19 @@ const search = () => {
   return (
     <div id='search'>
       <div className='search__inner'>
-        
+
+        <label htmlFor='searchInput'>
+          <span className='ir'>검색</span>
+        </label>
+
         <input 
             type='search'
             name='searchInput'
             id='searchInput'
             autoComplete='off'
-            className='search__inner'
-            placeholder='검색어를 입력해주세요!' />
-            <label htmlFor='searchInput'>
-            <span className='ir'>검색</span>
-        </label>
+            className='search__input'
+            placeholder='검색어를 입력해주세요!'
+        />
       </div>
     </div>
   )
